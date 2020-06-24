becbb52961fd83fea1b194f14ef95b04bfd7550a
zoupeng <zoupeng@58.com>
2020-06-24 15:29:11 +0800

    创建递归目录

--- src/copy.js
+++ src/copy.js
@@ -1,14 +1,41 @@
 var fs=require("fs");
 
+// 递归创建目录
+function mkdirSync(dir, cb) {
+    let paths = dir.split('/');
+    let index = 1;
+  
+    function next(index) {
+      if (index > paths.length) return cb();
+      let newPath = paths.slice(0, index).join('/');
+      fs.stat(newPath, function (err) {
+        if (err) {
+          fs.mkdir(newPath, function (err) {
+            next(index + 1);
+          });
+        } else {
+          next(index + 1);
+        }
+      })
+    }
+    next(index);
+
+}
 const writeFileRecursive = function(path, buffer, callback){
     let lastPath = path.substring(0, path.lastIndexOf("/"));
-    fs.mkdir(lastPath, {recursive: true}, (err) => {
-        if (err) return callback(err);
+    // fs.mkdir(lastPath, {recursive: true}, (err) => {
+    //     if (err) return callback(err);
+    //     fs.writeFile(path, buffer, function(err){
+    //         if (err) return callback(err);
+    //         return callback(null);
+    //     });
+    // });
+    mkdirSync(lastPath, function(){
         fs.writeFile(path, buffer, function(err){
             if (err) return callback(err);
             return callback(null);
         });
-    });
+    })
 }
 
 function copyFile(from, to){
